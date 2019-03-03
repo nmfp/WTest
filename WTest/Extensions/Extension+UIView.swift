31d5233d2c56accd9b86ac23cf6c4199170a5988
Nuno Pereira <nmfpereira@gmail.com>
2019-03-03 12:17:48 +0000

    Added an extension method to handle auto layout.

---
+++ WTest/Extensions/Extension+UIView.swift
@@ -0,0 +1,43 @@
+//
+//  Extension+UIView.swift
+//  WTest
+//
+//  Created by Nuno Pereira on 03/03/2019.
+//  Copyright © 2019 Nuno Pereira. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        
+    }
+}
+
